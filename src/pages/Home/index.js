e2145805ba578346f224994ec92d0acd365f73b8
Benjamin Modayil <modayilme@gmail.com>
2018-05-22 08:44:43 -0400

    fixed flex

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,8 +13,8 @@ export class Home extends Component {
       <React.Fragment>
         <main className="bg-red py-16 md-py-0">
           <div className="container p-4 md-p-2 lg-p-0 text-white mx-auto md-h-screen">
-            <div className="md-flex md-flex-col md-flex-row justify-between md--mx-8 lg--mx-16 items-center h-screen">
-              <div className="flex flex-col mb-8 md-mb-0 md-w-1-2 md-px-8 lg-px-16 max-w-md">
+            <div className="flex flex-col md-flex-row justify-between md--mx-8 lg--mx-16 items-center h-screen">
+              <div className="md-flex md-flex-col mb-8 md-mb-0 md-w-1-2 md-px-8 lg-px-16 max-w-md">
                 <h1 className="mt-0 text-5xl font-semibold leading-tight text-center md-text-left">
                   <span className="block text-xl italic font-normal">Do you have a </span>TaskToDo
                   <span className="block md-inline md-ml-4 text-xl italic">?</span>
